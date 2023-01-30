b7cbaa97922f1364c864506ab2132df91b2916ad
MushroomFX <dergolem26@gmail.com>
2023-01-30 17:42:31 +0100

    30.01.2023

--- push.bat
+++ push.bat
@@ -1,10 +1,10 @@
+echo "var data = `" > data.js
+tree /f >> data.js
+echo "`" >> data.js
+
 git pull
 git add -A
 git commit -m %date%
 git push
-
-echo "var data = `" > data.js
-tree /f >> data.js
-echo "`" > data.js
 
 timeout -1
