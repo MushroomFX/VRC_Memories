3d0167f8bcb4eeec40cf0b89f347915cd35b373a
MushroomFX <dergolem26@gmail.com>
2023-03-31 16:12:56 +0200

    31.03.2023

--- push.bat
+++ push.bat
@@ -1,9 +1,17 @@
+set current_dir=%cd%
 set source_dir=C:\Program Files (x86)\Steam\userdata\895010532\760\remote\438100\screenshots
 set dest_dir=%cd%\steam_screenshots
 
-for %%i in ("%source_dir%\*") do (
+for %%i in ("%source_dir%\*.jpg") do (
     echo %%i | findstr vr || move "%%i" "%dest_dir%"
 )
+
+pushd C:\Program Files (x86)\Steam\userdata\895010532\760\remote\438100\screenshots
+mkdir vr
+move * .\vr
+pushd %current_dir%
+
+timeout -1
 
 echo var data = ` > data.js
 tree /f /a >> data.js
