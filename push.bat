83d271e1f26d20db09be692f89fd7f0d409ceb39
MushroomFX <dergolem26@gmail.com>
2023-03-31 16:37:06 +0200

    31.03.2023

--- push.bat
+++ push.bat
@@ -32,7 +32,5 @@ git push
 
 
 set "file_contents="
-
-for /f "usebackq delims=" %%a in ("lastCommit.log") do set "file_contents=!file_contents!%%a"
-
+for /f "usebackq tokens=*" %%a in ("lastCommit.log") do set "file_contents=%file_contents%\n%%a"
 msg * %file_contents%
