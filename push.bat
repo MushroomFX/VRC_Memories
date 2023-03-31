87e8c82c49e25e889059cb9b9fed04ba67d72067
MushroomFX <dergolem26@gmail.com>
2023-03-31 15:56:43 +0200

    31.03.2023

--- push.bat
+++ push.bat
@@ -1,3 +1,13 @@
+set source_dir=C:\Program Files (x86)\Steam\userdata\895010532\760\remote\438100\screenshots
+set dest_dir=%cd%\steam_screenshots
+
+for %%i in ("%source_dir%\*") do (
+    if not "%%~xi"=="_vr.jpg" (
+        copy "%%i" "%dest_dir%"
+    )
+)
+
+
 echo var data = ` > data.js
 tree /f /a >> data.js
 echo ` >> data.js
